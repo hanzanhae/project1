578a5aeac067cadc5896169834194ab80d02cee4
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2024-09-26 18:55:33 +0900

    모달창 스크롤 수정

--- src/components/Modal/LikedMovies.js
+++ src/components/Modal/LikedMovies.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const LikedMovies = ({ setIsShowLiked, likedMovies }) => {
   const handleCloseModal = () => {
     setIsShowLiked(false);
+    document.body.style.overflow = "";
   };
   const handleModalEvent = (e) => {
     e.stopPropagation();
