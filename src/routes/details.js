2b94b56dd88db07b26711c8aee27ca102da78e8d
Gnar-dev <121690671+Gnar-dev@users.noreply.github.com>
2024-09-27 16:06:08 +0900

    Update details.js

--- src/routes/details.js
+++ src/routes/details.js
@@ -1,12 +1,1 @@
-import { useParams, useEffect } from "react";
 
-function Detail() {
-  // 컴포넌트 이름을 대문자로 수정
-  const { id } = useParams();
-
-  useEffect(() => {}, [id]);
-
-  return <div>Movie Detail for {id}</div>;
-}
-
-export default Detail;
